360f729c7317479dbc1cd1d7f392d3167b70714e
Mark S. Maglana <mmaglana@gmail.com>
2016-03-24 11:02:14 -0700

    Use Object#get instead of Kernel::open to get object from backend
    This change uses the object instance directly to get the file from the
    backend instead of using a presigned URL to get it. Presigning was not
    necessary in this method since the URL doesn't get passed on to the
    client anyway. Also, by avoiding Kernel::open, we avoid having to
    manage certificate stores in both `aws-sdk` and `open-uri`.

--- lib/refile/s3.rb
+++ lib/refile/s3.rb
@@ -95,7 +95,9 @@ module Refile
     # @param [String] id           The id of the file
     # @return [IO]                An IO object containing the file contents
     verify_id def open(id)
-      Kernel.open(object(id).presigned_url(:get))
+      object(id).get.body
+    rescue Aws::S3::Errors::NoSuchKey
+      nil
     end
 
     # Return the entire contents of the uploaded file as a String.
